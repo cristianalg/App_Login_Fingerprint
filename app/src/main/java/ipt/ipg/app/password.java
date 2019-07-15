9101b5e46b840cbab12a1288cab9bcf90d5dec68
Cristiana <cristianalg7@hotmail.com>
2019-07-15 18:29:06 +0100

    Definido o Layout do Main e o layout de password
    Definido o Layout, Main, de escolher se é para iniciar login ou criar o primerio registo e o layout de  login com password, mas não contêm ações.

---
+++ app/src/main/java/ipt/ipg/app/password.java
@@ -0,0 +1,14 @@
+package ipt.ipg.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password);
+    }
+}
