7f5de90422a02f392a37ec3c2152f7e6d67e7cf5
Cristiana <cristianalg7@hotmail.com>
2019-07-15 19:28:42 +0100

    Atualizar a Class MainActivity
    Através dos botões de Login e SignUP chamar as respetivas classes.

--- app/src/main/java/ipt/ipg/app/MainActivity.java
+++ app/src/main/java/ipt/ipg/app/MainActivity.java
@@ -1,14 +1,51 @@
 package ipt.ipg.app;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(null != actionBar){
+            actionBar.hide();
+        }
+
+
+        Button signInButton = (Button)findViewById(R.id.sign_in);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        Button signUpButton = (Button)findViewById(R.id.sign_up);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(signInIntent);
+            }
+        });
     }
 }
